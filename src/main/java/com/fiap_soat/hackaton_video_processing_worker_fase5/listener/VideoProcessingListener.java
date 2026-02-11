9037cedc7186ec1e70651f6ec8e9758188dd4875
mariomendonca <mariomeendonca00@gmail.com>
2026-02-11 08:02:34 -0300

    convert message to java class

--- src/main/java/com/fiap_soat/hackaton_video_processing_worker_fase5/listener/VideoProcessingListener.java
+++ src/main/java/com/fiap_soat/hackaton_video_processing_worker_fase5/listener/VideoProcessingListener.java
@@ -1,19 +1,33 @@
 package com.fiap_soat.hackaton_video_processing_worker_fase5.listener;
 
 import com.fiap_soat.hackaton_video_processing_worker_fase5.domain.dto.VideoProcessingRequest;
+import com.fiap_soat.hackaton_video_processing_worker_fase5.service.VideoProcessingService;
 import lombok.AllArgsConstructor;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 @AllArgsConstructor
 public class VideoProcessingListener {
 
-    private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
+    private final VideoProcessingService videoProcessingService;
 
     @RabbitListener(queues = "video.processing.queue")
-    public void receiveMessage(VideoProcessingRequest message) {
-        System.out.println("Received message: " + message);
+    public void receiveMessage(Message message) {
+        try {
+            VideoProcessingRequest request = objectMapper.readValue(
+                message.getBody(),
+                VideoProcessingRequest.class
+            );
+
+            videoProcessingService.processVideo(request);
+        } catch (Exception e) {
+            throw new AmqpRejectAndDontRequeueException("Invalid message payload", e);
+        }
     }
 }
---
+++ src/main/java/com/fiap_soat/hackaton_video_processing_worker_fase5/service/VideoProcessingServiceImpl.java
@@ -0,0 +1,15 @@
+package com.fiap_soat.hackaton_video_processing_worker_fase5.service;
+
+import com.fiap_soat.hackaton_video_processing_worker_fase5.domain.dto.VideoProcessingRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class VideoProcessingServiceImpl implements VideoProcessingService {
+
+    @Override
+    public void processVideo(VideoProcessingRequest videoProcessingRequest) {
+        System.out.println(videoProcessingRequest);
+    }
+}
